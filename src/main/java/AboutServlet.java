209d3a27b3ea532efc373437ad83ddf51bb275bc
V. Kukuyashnyy <starsonic71@gmail.com>
2020-11-12 21:38:07 +0200

    Initial commit

---
+++ src/main/java/AboutServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AboutServlet extends HttpServlet {
+    @Override
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, res);
+        log("Invoke");
+
+        if("GET".equals(req.getMethod())) {
+            resp.getWriter().println("<a href='/'>Home</a>");
+        }
+    }
+}
